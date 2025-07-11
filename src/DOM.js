65e4bfc671a08568cccd8e6aaede5a31b7fd05d0
Kashif <nizamanikashif2003@gmail.com>
2025-07-11 13:24:25 +0500

    refactored code for modularity

--- src/DOM.js
+++ src/DOM.js
@@ -1,33 +1,99 @@
+import Project from "./projects";
 
 const sidebar = document.querySelector(".projectList");
 const main_content = document.querySelector(".todoList");
 
- export const project_list = [];
+const project_dialog = document.querySelector(".projects");
+const todo_dialog = document.querySelector(".todos");
 
+const activeClass = "active";
 
-export function displayProjects(project){
-    project_list.push(project)
-    const project_container = document.createElement("div");
-    project_container.classList.add("project_item")
-    project_container.setAttribute("data-id", project.id)
-      if(project.name === "Default")
-         project_container.classList.add("active")
-       project_container.innerHTML = "<h2>" + project.name + " </h2>";
-       sidebar.appendChild(project_container);
+export const project_list = [];
 
+export function displayProjects(project) {
+  project_list.push(project);
+  const project_container = document.createElement("div");
+  project_container.classList.add("project_item");
+  project_container.setAttribute("data-id", project.id);
+  if (project.name === "Default") project_container.classList.add("active");
+  project_container.innerHTML = "<h2>" + project.name + " </h2>";
+  sidebar.appendChild(project_container);
 }
 
+export function displaytodos(todos) {
+  main_content.innerHTML = "";
 
- export function displaytodos(todos){
-      main_content.innerHTML = "";
-      console.log(todos);
-    todos.forEach(element => {
-      
-        const todo = document.createElement("div");
-        todo.classList.add("todo_item")
-        todo.innerHTML = "<h2>" + element.title + "</h2>" + "<div>" + element.description + "</div> <br>" + element.dueDate ;
-        main_content.appendChild(todo);
-    });
+  todos.forEach((element) => {
+    const todo = document.createElement("div");
+    todo.classList.add("todo_item");
+    todo.innerHTML =
+      "<h2>" +
+      element.title +
+      "</h2>" +
+      "<div>" +
+      element.description +
+      "</div> <br>" +
+      element.dueDate;
+    main_content.appendChild(todo);
+  });
 }
 
+export function openDialog(e) {
+  if (e.target.classList[1] === "project") project_dialog.showModal();
+  else todo_dialog.showModal();
+}
 
+export function closeDialog(e) {
+  if (e.target.classList[1] === "project") project_dialog.close();
+  else todo_dialog.close();
+}
+
+export function getActiveProject() {
+  const current_project = document.querySelector(".project_item.active");
+  const currentId = current_project.getAttribute("data-id");
+
+  const project = project_list.find((p) => p.id === currentId); 
+  return project;
+}
+
+export function createDOMproject() {
+  const name = document.querySelector("#name");
+  const project = new Project(name.value);
+  project_dialog.close();
+  displayProjects(project);
+}
+
+export function createDOMtodo() {
+  const title = document.querySelector("#title");
+  const dueDate = document.querySelector("#dueDate");
+  const description = document.querySelector("#description");
+  const priority = document.querySelector("#priority");
+
+  const activeProject = getActiveProject();
+
+  getActiveProject().createTodo(
+    title.value,
+    dueDate.value,
+    description.value,
+    priority.value
+  );
+  todo_dialog.close();
+  displaytodos(activeProject.todos);
+}
+
+export function setActiveProject(event) {
+  const clicked = event.target.closest(".project_item");
+  if (!clicked) return; // Ignore clicks outside .project_item
+
+  // Remove 'active' from all
+  sidebar.querySelectorAll(".project_item").forEach((item) => {
+    item.classList.remove(activeClass);
+  });
+
+  // Added 'active' to the clicked one
+  clicked.classList.add(activeClass);
+  project_list.forEach((element) => {
+    if (element.id === clicked.getAttribute("data-id"))
+      displaytodos(element.todos);
+  });
+}
--- src/eventHandler.js
+++ src/eventHandler.js
@@ -1,105 +1,46 @@
-import { displayProjects, project_list, displaytodos } from "./DOM";
-import Project from "./projects";
+import {
+
+  openDialog,
+  closeDialog,
+  createDOMproject,
+  createDOMtodo,
+  setActiveProject,
+} from "./DOM";
+
 
 const add_btn = document.querySelectorAll(".add");
-const project_dialog = document.querySelector(".projects");
-const todo_dialog = document.querySelector(".todos");
 const close_dialog = document.querySelectorAll(".close");
-const submit_dialog = document.querySelectorAll(".submit")
+const submit_dialog = document.querySelectorAll(".submit");
 
 const sidebar = document.querySelector(".projectList");
-const activeClass = "active";
 
 
-
-
-
-
-
-add_btn.forEach((e)=>{
-
-    e.addEventListener("click", (e)=>{
-     
-
-
-
-        if(e.target.classList[1] === "project")
-            project_dialog.showModal();
-        else
-           todo_dialog.showModal();
-
-    })
-
-})
-
-close_dialog.forEach((e)=> {
-    e.addEventListener("click", (e)=> {
-
-        if(e.target.classList[1] === "project")
-            project_dialog.close();
-        else
-           todo_dialog.close();
-
-    })
-})
-
-submit_dialog.forEach((e)=>{
-        e.addEventListener("click", (e)=>{
-
-            e.preventDefault();
-
-            if(e.target.classList[1] === "project"){
-            const name = document.querySelector("#name");
-            const project = new Project(name.value);
-            project_dialog.close();
-            displayProjects(project);
-            }
-            else{
-              
-           const current_project = document.querySelector(".project_item.active");
-           console.log(current_project);
-           console.log(project_list)
-           const title = document.querySelector("#title");
-           const dueDate = document.querySelector("#dueDate");
-           const description = document.querySelector("#description");
-           const priority = document.querySelector("#priority")
-           project_list.forEach((e)=>{
-            
-            if(e.id === current_project.getAttribute("data-id")){
-               const todo = e.createTodo(title.value, dueDate.value, description.value, priority.value);
-                todo_dialog.close()
-                displaytodos(e.todos);
-
-                //compare data id to get the correct project and then make todos in it
-            }
-
-           })
-            
-
-            }
-
-        })
-})
-
-
-
-
-// Delegated event listener (works for dynamically added .project_item)
-sidebar.addEventListener("click", (event) => {
-  const clicked = event.target.closest(".project_item");
-  if (!clicked) return; // Ignore clicks outside .project_item
-
-  // Remove 'active' from all
-  sidebar.querySelectorAll(".project_item").forEach(item => {
-    item.classList.remove(activeClass);
+add_btn.forEach((e) => {
+  e.addEventListener("click", (e) => {
+    openDialog(e);
   });
-
-  // Added 'active' to the clicked one
-  clicked.classList.add(activeClass);
-  project_list.forEach((element)=>{
-    if(element.id === clicked.getAttribute("data-id"));
-       displaytodos(element.todos);
-  })
-  
 });
 
+close_dialog.forEach((e) => {
+  e.addEventListener("click", (e) => {
+    closeDialog(e);
+  });
+});
+
+submit_dialog.forEach((e) => {
+  e.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    if (e.target.classList[1] === "project") {
+      createDOMproject();
+    } else {
+      createDOMtodo();
+    }
+  });
+});
+
+
+sidebar.addEventListener("click", (event) => {
+    
+  setActiveProject(event);
+});
